ff72fbcf0d47dff34f5fa2ac86f6a8a1feb02e29
MrKyrylo <kirilka294729@gmail.com>
2023-12-11 23:09:58 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -6,21 +6,26 @@ function getShippingCost(country) {
         case "China":
             price = 100;
             message = `Shipping to ${country} will cost ${price} credits`;
+            return message;
             break;
         case "Chile":
             price = 250;
             message = `Shipping to ${country} will cost ${price} credits`;
+            return message;
             break;
         case "Australia":
             price = 170;
             message = `Shipping to ${country} will cost ${price} credits`;
+            return message;
             break;
         case "Jamaica":
             price = 120;
             message = `Shipping to ${country} will cost ${price} credits`;
+            return message;
             break;
         default:
             message = "Sorry, there is no delivery to your country";
+            return message;
             break;
     }
 }
